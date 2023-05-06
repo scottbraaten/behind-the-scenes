32061218cfe4ce3fa9e8e958c4b7f7e1d79c3dcc
Scott Braaten <scottnicholasbraaten@gmail.com>
2023-05-06 01:09:12 -0500

    useCallback and React.memo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
+  const [para, setPara] = useState(false);
+  const [allow, setAllow] = useState(false);
+
+  console.log('App');
+
+  const toggleHandler = useCallback(() => {
+    if (allow) {
+      setPara((prevPara) => !prevPara);
+    }
+  }, [allow]);
+
+  const allowHandler = () => {
+    setAllow(true);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={para} />
+      <Button onClick={allowHandler}>Allow toggling</Button>
+      <Button onClick={toggleHandler}>Toggle paragraph!</Button>
     </div>
   );
 }
---
+++ src/components/Demo/MyParagraph.js
@@ -0,0 +1,8 @@
+const MyParagraph = props => {
+    console.log('mypara')
+    return (
+        <p>{props.children}</p>
+    )
+}
+
+export default MyParagraph;
